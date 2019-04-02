fdf1db4a5d2236a7ce0e516a265f7da7eba43823
MichalPutos <michal.putos@gmail.com>
2019-04-03 01:40:45 +0200

    working with occasional fall to infinite loop - to be repaired

--- getch.py
+++ getch.py
@@ -1,30 +1,19 @@
-import msvcrt
-
-
 class _Getch:
-    """Gets a single character from standard input.  Does not echo to the
-screen. From http://code.activestate.com/recipes/134892/"""
+    """Gets a single character from standard input.  Does not echo to the screen."""
 
     def __init__(self):
         self.impl = _GetchWindows()
 
-    def __call__(self):
-        return self.impl()
+    def __call__(self): return self.impl()
 
 
 class _GetchWindows:
     def __init__(self):
-        pass
+        import msvcrt
 
     def __call__(self):
+        import msvcrt
         return msvcrt.getch()
 
 
-def getKey():
-    inkey = _Getch()
-    while True:
-        k = inkey()
-        if k != '':
-            break
-
-    return k.decode("utf-8")
+getch = _Getch()
