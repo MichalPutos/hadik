157723ffc1cdccb94723cf5b2263fbf8c5d253ed
MichalPutos <michal.putos@gmail.com>
2019-04-11 15:12:52 +0200

    working with occasional fall to infinite loop - to be repaired

--- realtime_getch.py
+++ realtime_getch.py
@@ -1,4 +1,3 @@
-import time
 import msvcrt
 import threading
 
@@ -13,10 +12,3 @@ class RealTimeyKey:
     def press_key(self):
         while True:
             self.char = msvcrt.getch().decode("utf-8")
-
-#
-# key_watcher = RealTimeyKey()
-#
-# while True:
-#     time.sleep(0.1)
-#     print(key_watcher.char)
